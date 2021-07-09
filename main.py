30c07a124de117d3a7ab166dca6115cceca09fea
1001139766daniel <87156974+1001139766daniel@users.noreply.github.com>
2021-07-08 21:34:57 -0500

    v2 - Con Database
    Se agregan tareas de BDD

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import objects
+import DataBase
 
 nJugadores = int(input("¿Cuantos jugadores habrán? \n"))
 distancia = float(input("¿Cuantos kilometros tendrá la pista? \n"))
@@ -25,9 +26,19 @@ pista = objects.Pista(nJugadores, distancia, listJugadores)
 
 juego = objects.Juego(pista)
 print("****************************************************** Inicio del Juego ******************************************************")
-juego.partida()
+ganadores = juego.partida()
+print("****************************************************** FIN del Juego **************************************\n")
+createDataBase = int(input("¿Si desea crear la base de datos ingrese 1? (Si ya está creada e ingresa 1 genera error) \n"))
+if createDataBase==1:
+    DataBase.createDataBase()
 
-
-
-
-
+cont=0
+for ganador in ganadores:
+    cont=cont+1
+    if cont==1:
+        DataBase.insertResults(ganador.nombre, "1", "0", "0")
+    if cont==2:
+        DataBase.insertResults(ganador.nombre, "0", "1", "0")
+    if cont==3:
+        DataBase.insertResults(ganador.nombre, "0", "0", "1")
+    DataBase.consulta(ganador.nombre)
